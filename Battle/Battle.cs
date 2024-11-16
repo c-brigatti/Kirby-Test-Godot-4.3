f09c2d3b98542644f73bde4f69f047e3c23c383c
Cameron Brigatti <brigatti.cameron@gmail.com>
2024-11-17 06:35:07 +0800

    working Solution file. Basic signals working

--- Battle/Battle.cs
+++ Battle/Battle.cs
@@ -5,15 +5,29 @@ public partial class Battle : Control
 {
 	private WindowBox _options;
 	private WindowBox _choices;
-	private Menu _options_menu;
-	private Menu _choices_menu;
+	private Menu _optionsMenu;
+	private Menu _choicesMenu;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_options = GetNode<WindowBox>("MarginContainer/VBoxContainer/Bottom/OptionsBox");
 		_choices = GetNode<WindowBox>("MarginContainer/VBoxContainer/Bottom/ChoicesBox");
-		_options_menu = GetNode<Menu>("MarginContainer/VBoxContainer/Bottom/OptionsBox/OptionsScroller/Options");
-		_choices_menu = GetNode<Menu>("MarginContainer/VBoxContainer/Bottom/ChoicesBox/ChoicesScroller/Choices");
+		_optionsMenu = GetNode<Menu>("MarginContainer/VBoxContainer/Bottom/OptionsBox/OptionsScroller/Options");
+		_choicesMenu = GetNode<Menu>("MarginContainer/VBoxContainer/Bottom/ChoicesBox/ChoicesScroller/Choices");
+
+		_optionsMenu.FocusButton(0);
+	}
+
+	void _on_options_button_focused(BaseButton button)
+	{
+		if (button is Button castButton)
+			GD.Print(castButton.Text + " focused");
+	}
+	
+	void _on_options_button_pressed(BaseButton button)
+	{
+		if (button is Button castButton)
+			GD.Print(castButton.Text + " pressed");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- Battle/Menu.cs
+++ Battle/Menu.cs
@@ -4,8 +4,51 @@ using System.Linq;
 
 public partial class Menu : Container
 {
-	Godot.Collections.Array<Button> get_buttons()
+	[Signal]
+	public delegate void ButtonFocusedEventHandler(BaseButton button);
+
+	[Signal]
+	public delegate void ButtonPressedEventHandler(BaseButton button);
+
+	private int index = 0;
+	
+	public override void _Ready()
 	{
-		return GetChildren().OfType<Button>;
+		foreach (var button in GetButtons())
+		{
+			button.FocusEntered += () => OnButtonFocused(button);
+			button.Pressed += () => OnButtonPressed(button);
+		}
+	}
+
+	// Retrieve all Button children
+	private Godot.Collections.Array<BaseButton> GetButtons()
+	{
+		return new Godot.Collections.Array<BaseButton>(GetChildren().OfType<BaseButton>());
+	}
+
+	// Connect signals to all buttons
+	private void ConnectToButtons()
+	{
+
+	}
+
+	// Focus a button by index
+	public void FocusButton(int focusIndex = 0)
+	{
+		var buttons = GetButtons();
+		buttons[focusIndex].GrabFocus();
+	}
+
+	// Emit signal when a button gains focus
+	private void OnButtonFocused(BaseButton button)
+	{
+		EmitSignal("ButtonFocused", button);
+	}
+
+	// Emit signal when a button is pressed
+	private void OnButtonPressed(BaseButton button)
+	{
+		EmitSignal("ButtonPressed", button);
 	}
 }
